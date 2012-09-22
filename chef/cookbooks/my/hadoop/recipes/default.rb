8152e9c2545da8afbe16cd0a510a9b84518226ff
Mateusz Haligowski <mhaligowski@gmail.com>
2012-09-22 19:21:06 +0200

    download and unpack hadoop

--- chef/cookbooks/my/hadoop/recipes/default.rb
+++ chef/cookbooks/my/hadoop/recipes/default.rb
@@ -12,9 +12,24 @@ user "hduser" do
   system false
   password "$1$Q5YyJZpE$VePKWkZjH0RUq9L2AUx/I/"
   shell "/bin/bash"
+  home "/home/hduser"
 end
 
 group "hdgroup" do
 	members ["hduser"]
 	append true
+end
+
+remote_file "/tmp/hadoop.tar.gz" do
+	source "http://ftp.ps.pl/pub/apache/hadoop/common/alpha/hadoop-2.0.1-alpha.tar.gz"
+end
+
+script "install_hadoop" do
+	interpreter "bash"
+	cwd "/tmp"
+	code <<-EOH
+		tar zxf hadoop.tar.gz -C /usr/local/
+		ln -s /usr/local/hadoop-2.0.1-alpha/ /usr/local/hadoop
+		chown -R hduser:hdgroup /usr/local/hadoop-2.0.1-alpha  
+	EOH
 end
