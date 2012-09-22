bc94d618fac329664e31ff03705bdf2816319f42
Mateusz Haligowski <mhaligowski@gmail.com>
2012-09-22 23:55:23 +0200

    single node config is done!

--- chef/cookbooks/my/hadoop/recipes/default.rb
+++ chef/cookbooks/my/hadoop/recipes/default.rb
@@ -7,17 +7,15 @@
 # All rights reserved - Do Not Redistribute
 #
 
-user "hduser" do
-  comment "Hadoop User"
-  system false
-  password "$1$Q5YyJZpE$VePKWkZjH0RUq9L2AUx/I/"
-  shell "/bin/bash"
-  home "/home/hduser"
-end
-
-group "hdgroup" do
-	members ["hduser"]
-	append true
+template "/etc/sudoers" do
+  source "sudoers.erb"
+  mode 0440
+  owner "root"
+  group "root"
+  variables({
+    :sudoers_groups => node[:authorization][:sudo][:groups],
+    :sudoers_users => node[:authorization][:sudo][:users]
+  })
 end
 
 remote_file "/tmp/hadoop.tar.gz" do
@@ -30,6 +28,27 @@ script "install_hadoop" do
 	code <<-EOH
 		tar zxf hadoop.tar.gz -C /usr/local/
 		ln -s /usr/local/hadoop-2.0.1-alpha/ /usr/local/hadoop
-		chown -R hduser:hdgroup /usr/local/hadoop-2.0.1-alpha  
+	EOH
+end
+
+script "generate_key" do
+  interpreter "bash"
+  code <<-EOH
+  	ssh-keygen -t rsa -q -f $HOME/.ssh/id_rsa -P ""
+  	cat $HOME/.ssh/id_rsa.pub >> $HOME/.ssh/authorized_keys
+  	ssh-keyscan -t rsa localhost >> $HOME/.ssh/known_hosts
+  	ssh-keyscan -t rsa 0.0.0.0 >> $HOME/.ssh/known_hosts
+  EOH
+end
+
+template "/usr/local/hadoop/etc/hadoop/hadoop-env.sh" do
+	source "hadoop-env.sh.erb"
+	variables({ :java_home => node['java']['java_home']})
+end
+
+script "configure_hadoop" do
+	interpreter "bash"
+	code <<-EOH
+		/usr/local/hadoop/sbin/update-hadoop-env.sh --prefix=/usr/local/hadoop
 	EOH
 end
